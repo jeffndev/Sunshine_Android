938fa02fcb7cb9a64a5a061d560e3db019f92056
unknown <jnewel200@PAWCHL-GCWDF12.cable.comcast.com>
2015-04-23 15:22:11 -0400

    plumbing for all this sync adapter stuff

---
+++ app/src/main/java/com/example/jnewel200/sunshine/app/sync/SunshineSyncService.java
@@ -0,0 +1,29 @@
+package com.example.jnewel200.sunshine.app.sync;
+
+/**
+ * Created by jnewel200 on 4/23/2015.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SunshineSyncService", "onCreate - SunshineSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
